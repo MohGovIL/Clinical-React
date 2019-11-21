b307346a05d79244dda23bddf820ce3e262c61d9
Amiel Elboim <amielel@matrix.co.il>
2019-11-21 12:05:48 +0200

    poc

--- clinikal-react/src/App.js
+++ clinikal-react/src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from "./Header";
+import IframeEnvelopPatientFile from "./IframeEnvelopPatientFile";
+// import i18n (needs to be bundled ;))
+import './i18n';
+import { Trans } from 'react-i18next';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            iframeUrl:''
+        };
+
+    }
+
+
+    changeUrl(page) {
+        this.setState({
+            iframeUrl: this.props.appUrl + '/' + page
+        })
+    }
+
+    render() {
+        return <div className="App">
+                <Header height="200px">
+                    <button onClick={e => this.changeUrl('/main/finder/dynamic_finder.php')}>Patient list</button>
+                    <button onClick={e => this.changeUrl('patient_file/summary/demographics.php?set_pid=2')}><Trans>Demographics</Trans></button>
+                    <button onClick={e => this.changeUrl('patient_file/encounter/forms.php?review_id=9')}>Encounter</button>
+                </Header>
+                <body>
+                    <IframeEnvelopPatientFile url={this.state.iframeUrl} />
+
+                </body>
+            </div>
+    }
+
 }
 
 export default App;
--- clinikal-react/src/index.js
+++ clinikal-react/src/index.js
@@ -4,7 +4,17 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const urlParams = new URLSearchParams(window.location.search);
+const envMode = urlParams.get('mode');
+var iframePath;
+if (typeof envMode !== "undefined" && envMode == 'dev') {
+    const basePath = urlParams.get('path');
+    iframePath = `http://localhost${basePath}/../openemr/interface`;
+} else {
+    iframePath = 'https://www.hugofranca.com/404';
+}
+
+ReactDOM.render(<App appUrl={iframePath}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
