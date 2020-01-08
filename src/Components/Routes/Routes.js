ed22815b9d90c8ef5b407200bf3f95b851c0acec
idangi <idangi@matrix.co.il>
2020-01-08 09:54:53 +0200

    Nothing

--- src/Components/Routes/Routes.js
+++ src/Components/Routes/Routes.js
@@ -6,16 +6,17 @@ import Login from "../Login/Login";
 import LoginWithCSRF from "../Login/LoginWithCSRF";
 import InitApp from "../Generic/InitApp/InitApp";
 import {stateLessOrNot} from "../../Utils/Helpers/StatelessOrNot";
+import {basePath} from "../../Utils/Helpers/basePath";
 import Imaging from "../Imaging/Imaging";
 
 const Routes = (props) => {
     return (
-        <BrowserRouter>
+        <BrowserRouter basename={stateLessOrNot() ? '/' : basePath()} >
             <Switch>
                 {/*This route is for testing*/}
                 {/*<Route path='/' component={Imaging}/>*/}
                 {/*This route is for testing*/}
-                {console.log(props.history)}
+                {console.log(props)}
                 <Route exact path='/'
                        component={stateLessOrNot() ? Login : LoginWithCSRF}/>
                 <PrivateRoute exact path='/InitApp' component={InitApp} isAuth={props.isAuth}/>
