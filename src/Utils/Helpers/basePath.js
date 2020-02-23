61c7b0fbb4bd9d5d0488efb48c261aba727a3e26
Yuriy Gershem <yuriyge@matrix.co.il>
2020-02-23 15:20:07 +0200

    remove console.log

--- src/Utils/Helpers/basePath.js
+++ src/Utils/Helpers/basePath.js
@@ -3,6 +3,5 @@
  * @returns {string} Returns the base path of the projects
  */
 export const basePath = () => {
-    console.log(process.env);
         return `${window.location.protocol}//${window.location.host}/${process.env.REACT_APP_INSTALL_NAME}/openemr/`;
 };
