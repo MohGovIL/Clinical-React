5b438a30320848d896c35c3ebab26be28464c396
Dror Golan <drorgo@matrix.co.il>
2020-02-24 15:32:52 +0200

    First part PC-90

--- src/Assets/Elements/CustomizedTable/CustomizedTableButton/index.js
+++ src/Assets/Elements/CustomizedTable/CustomizedTableButton/index.js
@@ -4,8 +4,9 @@ import {useTranslation} from "react-i18next";
 
 const CustomizedTableButton = ({variant, color, label, onClickHandler,mode}) => {
     const {t} = useTranslation();
+    console.log("mode = "+ mode);
     return (
-        <StyledButton variant={variant} color={color} onClick={onClickHandler} disabled={mode !== 'write' ? false : true}>
+        <StyledButton variant={variant} color={color} onClick={onClickHandler} disabled={mode !== 'write' ? true : false}>
             {t(label)}
         </StyledButton>
     );
