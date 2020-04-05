f018f6604d85b8e3150b4e3c20b3b54ca21584ec
yuriyge <yuriyge@matrix.co.il>
2020-04-06 00:28:38 +0300

    PC-501 -> PC-503 little fixes to popup storie

--- src/Assets/Elements/CustomizedPopup/Popup.stories.js
+++ src/Assets/Elements/CustomizedPopup/Popup.stories.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState}  from "react";
 import CustomizedPopup from "./index";
 import {StylesProvider} from '@material-ui/core/styles'
 import GlobalStyle from "../../Themes/GlobalStyle";
@@ -6,7 +6,7 @@ import {withKnobs, object, select, text, boolean} from "@storybook/addon-knobs";
 import {store} from "../../../index";
 import ProviderWrapper from "../../../../.storybook/Provider";
 import {Button, Typography} from "@material-ui/core";
-import {useState} from "@storybook/addons";
+// import {useState} from "@storybook/addons";
 
 export default {
     title: 'Popup',
@@ -20,6 +20,8 @@ export default {
 export const normalCustomizedPopup = () => {
     const lang_dir = select('state.settings.lang_dir', {"rtl": "rtl", "ltr": "ltr"}, 'rtl');
     const withSubPopup = boolean('withSubPopup', false);
+    const contentPopup = text("content for popup component", ""); //inner text of component
+
     store.getState().settings.lang_dir = lang_dir;
 
     const [popupOpen, setPopupOpen] = useState(false);
@@ -43,7 +45,7 @@ export const normalCustomizedPopup = () => {
         'label': 'Undo editing',
         'variant': "text",
         'color': "primary",
-        'onClickHandler': handlePopupClose
+        'onClickHandler': handlePopupClose //user function
     }, {
         'label': 'save',
         'variant': "contained",
@@ -59,18 +61,19 @@ export const normalCustomizedPopup = () => {
                          title={text('title', 'Patient details')}
                          content_dividers={boolean('content_dividers', false)}
                          bottomButtons={object('bottomButtons', [...bottomButtonsData])}
-        ><Typography gutterBottom>
-            LibreOffice auto-generate text: dt and F3<br/>
-        </Typography>
+        >{contentPopup ? contentPopup : <Typography gutterBottom>
+            Customized popup component content<br/>
+        </Typography>}
+
             {withSubPopup &&
-            <>
+            <><br/>
                 <Button variant="outlined" color="primary" onClick={handlePopupNextOpen}>
                     Open next dialog
                 </Button>
                 <CustomizedPopup isOpen={popupNextOpen} onClose={handlePopupNextClose}
                                  title={text('Sub Popup[title]','Sub Popup title')}
                 >
-                    customized popup component
+                    Customized popup sub-component content
                 </CustomizedPopup>
             </>
             }
