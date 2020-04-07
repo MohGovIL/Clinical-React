7394f488a2ec18a973b65ddb26873a3afccb7c02
idangi <idangi@matrix.co.il>
2020-04-07 12:31:29 +0300

    HeaderTabs

--- src/Assets/Elements/Header/HeaderTabs/Style.js
+++ src/Assets/Elements/Header/HeaderTabs/Style.js
@@ -1,26 +1,6 @@
-import Tabs from "@material-ui/core/Tabs";
-import styled from "styled-components";
-// import React from "react";
-import {devicesValue} from "../../../Themes/BreakPoints";
-
-// export default styled(({...other}) => <Tabs {...other} />)`
-//   margin: 0;
-//   & .MuiTabs-indicator {
-//     color: #ffffff;
-//     border: solid 2px #ffffff;
-//     background-color: #ffffff;
-//     border-radius: 5px;
-//   }
-//
-//   & .Mui-selected {
-//   font-weight: bold;
-//   }
-//
-//   @media (min-width: ${devicesValue.desktop}px){
-//       margin: 0 60px;
-//
-//   }
-// `;
+import Tabs from '@material-ui/core/Tabs';
+import styled from 'styled-components';
+import { devicesValue } from '../../../Themes/BreakPoints';
 
 export default styled(Tabs)`
   margin: 0;
@@ -32,11 +12,10 @@ export default styled(Tabs)`
   }
 
   & .Mui-selected {
-  font-weight: bold;
+    font-weight: bold;
   }
 
-  @media (min-width: ${devicesValue.desktop}px){
-      margin: 0 60px;
-
+  @media (min-width: ${devicesValue.desktop}px) {
+    margin: 0 60px;
   }
 `;
--- src/Assets/Elements/Header/HeaderTabs/index.js
+++ src/Assets/Elements/Header/HeaderTabs/index.js
@@ -3,7 +3,7 @@ import HeaderTab from "./HeaderTab";
 import StyledTabs from './Style';
 
 
-const HeaderTabs = (props) => {
+const HeaderTabs = ({Items}) => {
     const [tabsValue, setTabsValue] = useState(0);
 
     const tabsHandler = (tabIndex) => {
@@ -12,7 +12,7 @@ const HeaderTabs = (props) => {
 
     return (
         <StyledTabs value={tabsValue}>
-            {props.Items.map((item, itemIndex) => <HeaderTab Label={item.label} Link={item.url}
+            {Items.map((item, itemIndex) => <HeaderTab Label={item.label} Link={item.url}
                                                              SubMenu={item.SubMenu}
                                                              key={item.menu_id}
                                                              tabIndex={itemIndex}
