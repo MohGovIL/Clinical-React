33a6a4b5394e1ca7e6c0d16ae0b24ec257343ac4
idangi <idangi@matrix.co.il>
2020-04-19 12:58:41 +0300

    added a new end-point for HMO

--- src/Utils/Services/FhirAPI.js
+++ src/Utils/Services/FhirAPI.js
@@ -159,6 +159,8 @@ export const getOrganizationTypeKupatHolim = () => {
     return fhirTokenInstance().get(`${fhirBasePath}/Organization?type=71`);
 };
 
+export const getHMO = HmoId => fhirTokenInstance().get(`${fhirBasePath}/Organization/${HmoId}`);
+
 export const getNextPrevAppointmentPerPatient = (date, patient,prev) =>{
     //PC-216 endpoint: /Appointment?date=ge<DATE>&_count=1&_sort=date&patient=<PID>&status:not=arrived&status:not=booked&status:not=cancelled
     try {
