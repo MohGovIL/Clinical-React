58a5c625768f8e3bc5f0b605bbeea82e66613ead
idangi <idangi@matrix.co.il>
2020-05-06 10:15:04 +0300

    Added denormalized for document reference PC-921

--- src/Utils/Helpers/FhirEntities/denormalizeFhirEntity/denormalizerFhirDocumentReference/index.js
+++ src/Utils/Helpers/FhirEntities/denormalizeFhirEntity/denormalizerFhirDocumentReference/index.js
@@ -4,12 +4,68 @@
  * @returns {object}
  */
 const denormalizerFhirDocumentReference = (documentReference) => {
+  const denormalizedDocumentReference = {};
+  let context = {};
+  let attachment = {};
   for (const documentReferenceKey in documentReference) {
     if (documentReference.hasOwnProperty(documentReferenceKey)) {
-      const element = documentReference[documentReferenceKey];
+      // documentReference[documentReferenceKey];
+      switch (documentReferenceKey) {
+        case 'categoryCode':
+          denormalizedDocumentReference['category'] = [
+            {
+              coding: [
+                {
+                  code: documentReference[documentReferenceKey],
+                },
+              ],
+            },
+          ];
+          break;
+        case 'author':
+          denormalizedDocumentReference['author'] = [
+            {
+              reference: `Practitioner/${documentReference[documentReferenceKey]}`,
+            },
+          ];
+          break;
+        case 'contentType':
+          attachment['contentType'] = documentReference[documentReferenceKey];
+          break;
+        case 'data':
+          attachment['data'] = documentReference[documentReferenceKey];
+          break;
+        case 'url':
+          attachment['url'] = documentReference[documentReferenceKey];
+          break;
+        case 'encounter':
+          context['encounter'] = [
+            {
+              reference: `Encounter/${documentReference[documentReferenceKey]}`,
+            },
+          ];
+          break;
+        case 'patient':
+          context['sourcePatientInfo'] = {
+            reference: `Patient/${documentReference[documentReferenceKey]}`,
+          };
+          break;
+        default:
+          break;
+      }
     }
   }
-  return {};
+  if (Object.values(context).length) {
+    denormalizedDocumentReference['context'] = context;
+  }
+  if (Object.values(attachment).length) {
+    denormalizedDocumentReference['content'] = [
+      {
+        attachment: attachment,
+      },
+    ];
+  }
+  return denormalizedDocumentReference;
 };
 
 export default denormalizerFhirDocumentReference;
