aa89c8ea1472a2bfb0f4d3a9363f13baef69f707
Dror Golan <drorgo@matrix.co.il>
2020-05-07 13:16:56 +0300

    FIX PC-927 ALSO REMOVE UNWANTED IMPORTS AND COMMENTED CODE

--- src/Assets/Elements/Header/Search/DrawThisTable/Style.js
+++ src/Assets/Elements/Header/Search/DrawThisTable/Style.js
@@ -1,74 +1,75 @@
-import styled from "styled-components";
-import {ExpansionPanelDetails, ExpansionPanelSummary} from "@material-ui/core";
-import ExpansionPanel from "@material-ui/core/ExpansionPanel";
-import GenderIcon from "Assets/Elements/CustomizedTable/CustomizedTablePersonalInformationCell/GenderIcon";
-import Icon from "@material-ui/core/Icon";
-import TitleValueComponent from "./TitleValueComponent";
-import Box from "@material-ui/core/Box";
-import Button from "@material-ui/core/Button";
+import styled from 'styled-components';
+import {
+  ExpansionPanelDetails,
+  ExpansionPanelSummary,
+} from '@material-ui/core';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import GenderIcon from 'Assets/Elements/CustomizedTable/CustomizedTablePersonalInformationCell/GenderIcon';
+import Icon from '@material-ui/core/Icon';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
 
 export const StyledExpansionPanel = styled(ExpansionPanel)`
-    margin: 0 !important;
-    position: inherit !important;
+  margin: 0 !important;
+  position: inherit !important;
 `;
 export const StyledExpansionPanelSummary = styled(ExpansionPanelSummary)`
-    display: flex;
-    max-height: 56px;
-    background-color: #ffffff;
-     box-shadow: 0px 0px 12px -1px #d4d4d4;
-    justify-content: space-evenly;
+  display: flex;
+  max-height: 56px;
+  background-color: #ffffff;
+  box-shadow: 0px 0px 12px -1px #d4d4d4;
+  justify-content: space-evenly;
 `;
 
 export const StyledExpansionPanelDetails = styled(ExpansionPanelDetails)`
-    width: 744px;
-    height: 100%;
-    display: block;
-    justify-content: stretch;
+  width: 744px;
+  height: 100%;
+  display: block;
+  justify-content: stretch;
 `;
 
 export const StyledBox = styled(Box)`
-    margin-top:24px;
-    /* margin-left: 24px;
+  margin-top: 24px;
+  /* margin-left: 24px;
     margin-right:28px;*/
-    width: 692px;
-    height: auto;
-    /*opacity: 0.2;*/
-    border: solid 1px #000b4024;
-    background-color: #f5f5f526;;
-    /*  justify-content: space-evenly;*/
-    /* display: flex;*/
+  width: 692px;
+  height: auto;
+  /*opacity: 0.2;*/
+  border: solid 1px #000b4024;
+  background-color: #f5f5f526;
+  /*  justify-content: space-evenly;*/
+  /* display: flex;*/
 `;
 export const StyledBottomLinks = styled(Box)`
-    margin: 37px 0px 24px 61px;
-    display: flex;
-    justify-content: flex-end;
-    a.MuiButton-containedPrimary{
-         background-color: #002398 !important;
-    }
-    a.MuiButton-contained.Mui-disabled{
-        opacity: 0.4 !important;
-        color: #ffffff !important;
-    }
-    a.MuiButton-outlined.MuiButton-outlinedPrimary.Mui-disabled{
+  margin: 37px 0px 24px 61px;
+  display: flex;
+  justify-content: flex-end;
+  a.MuiButton-containedPrimary {
+    background-color: #002398 !important;
+  }
+  a.MuiButton-contained.Mui-disabled {
+    opacity: 0.4 !important;
+    color: #ffffff !important;
+  }
+  a.MuiButton-outlined.MuiButton-outlinedPrimary.Mui-disabled {
     border: 1px solid #002398 !important;
     color: #002398 !important;
-    }
+  }
 `;
 export const StyledHrefTableButton = styled(Button)`
+  width: auto;
+  height: 32px;
+  border-radius: 25px;
 
-      width: auto;
-      height: 32px;
-      border-radius: 25px;
-
-      font-family: OpenSansHebrew;
-      font-size: 15px;
-      font-weight: normal;
-      font-stretch: normal;
-      font-style: normal;
-      line-height: normal;
-      letter-spacing: normal;
-      text-align: center;
-      color: #002398;
+  font-family: OpenSansHebrew;
+  font-size: 15px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  text-align: center;
+  color: #002398;
 `;
 export const StyledHrefButton = styled(Button)`
     border-radius: 25px;
@@ -91,300 +92,289 @@ export const StyledHrefButton = styled(Button)`
 `;
 
 export const StyledLabelAppointment = styled.label`
-    display: inline-flex;
-    /* max-width: 30%;
+  display: inline-flex;
+  /* max-width: 30%;
     min-width: 30%;
     height: 22px;*/
-    margin-top: 10px;
-    label{
-        margin-left: 10px;
-        margin-right: 0px;
-    }
-    span {
-        width: 86px;
-        height: 19px;
-        font-family: OpenSansHebrew;
-        font-size: 14px;
-        font-weight: bold;
-        font-stretch: normal;
-        font-style: normal;
-        line-height: normal;
-        letter-spacing: normal;
-        text-align: right;
-        color: #000b40;
-        margin-right:19px;
-    }
-    div{
-        width: 103px;
-        height: 19px;
-        font-family: OpenSansHebrew;
-        font-size: 14px;
-        font-weight: bold;
-        font-stretch: normal;
-        font-style: normal;
-        line-height: normal;
-        letter-spacing: normal;
-        text-align: right;
-         color: #000b40;
-    }
-`;
-
-export const StyledLabelStatusAppointment = styled.label`
-    display: inline-flex;
-    /* max-width: 30%;
-    min-width: 30%;
-    height: 22px;*/
-    margin-top: 10px;
-    label{
-        margin-left: 5px;
-        margin-right: -12px;
-    }
-    span {
-        width: 80px;
-        height: 18px;
-        font-family: OpenSansHebrew;
-        font-size: 14px;
-        font-weight: normal;
-        font-stretch: normal;
-        font-style: normal;
-        line-height: 1.29;
-        letter-spacing: normal;
-        text-align: right;
-
-        text-align: -webkit-center;
-        border-radius: 15px;
-        background :  #eaf7ff;
-    }
-    div{
-    width:50px;
-    }
-`;
-
-export const StyledLabelServiceTypeAppointment = styled.label`
-    display: inline-flex;
-    /* max-width: 30%;
-    min-width: 30%;
-    height: 22px;*/
-    margin-top: 10px;
-    label{
-        margin-left: 5px;
-        margin-right: -12px;
-    }
-    span {
-        width: 80px;
-        height: 18px;
-        font-family: OpenSansHebrew;
-        font-size: 14px;
-        font-weight: normal;
-        font-stretch: normal;
-        font-style: normal;
-        line-height: 1.29;
-        letter-spacing: normal;
-    }
-    div{
-
-    }
-`;
-
-export const StyledLabelName = styled.label`
-    display: inline-table;
-    max-width: 24%;
-    min-width: 24%;
-    height: 22px;
-    margin-top: 10px;
-    span{
-
-          font-family: OpenSansHebrew;
-          font-size: 14px;
-          font-weight: normal;
-          font-stretch: normal;
-          font-style: normal;
-          line-height: normal;
-          letter-spacing: normal;
-          text-align: right;
-          color: #00094a;
-          margin: auto;
-    }
-    div{
-
-          font-family: OpenSansHebrew;
-          font-size: 14px;
-          font-weight: normal;
-          font-stretch: normal;
-          font-style: normal;
-          line-height: normal;
-          letter-spacing: normal;
-          text-align: right;
-          color: #00094a;
-
-    }
-`;
-
-export const StyledLabelTZ = styled.label`
-    display: inline-flex;
-    max-width: 30%;
-    min-width: 30%;
-    height: 22px;
-    margin-top: 10px;
-    span{
-
-          font-family: OpenSansHebrew;
-          font-size: 16px;
-          font-weight: normal;
-          font-stretch: normal;
-          font-style: normal;
-          line-height: normal;
-          letter-spacing: normal;
-          text-align: right;
-          color: #000b40;
-    }
-    div{
-
-          font-family: OpenSansHebrew;
-          font-size: 16px;
-          font-weight: normal;
-          font-stretch: normal;
-          font-style: normal;
-          line-height: normal;
-          letter-spacing: normal;
-          text-align: right;
-          color: #000b40;
-    }
-`;
-
-
-export const StyledLabelPhone = styled.label`
-    display: inline-flex;
-    max-width: 30%;
-    min-width: 30%;
-    height: 22px;
-    margin-top: 10px;
-    span{
-        opacity: 0.6;
-        font-family: OpenSansHebrew;
-        font-size: 13px;
-        font-weight: normal;
-        font-stretch: normal;
-        font-style: normal;
-        line-height: 1.38;
-        letter-spacing: normal;
-        text-align: right;
-        color: #1e2132;
-    }
-    div{
-        font-family: OpenSansHebrew;
-        font-size: 16px;
-        font-weight: normal;
-        font-stretch: normal;
-        font-style: normal;
-        line-height: normal;
-        letter-spacing: normal;
-        text-align: right;
-        color: #00094a;
-    }
-`;
-
-
-export const StyledLabelAge = styled.label`
-    display: inline-flex;
-    max-width: 20%;
-    min-width: 20%;
-    height: 22px;
-    margin-top: 10px;
-    span{
-        opacity: 0.6;
-        font-family: OpenSansHebrew;
-        font-size: 13px;
-        font-weight: normal;
-        font-stretch: normal;
-        font-style: normal;
-        line-height: 1.38;
-        letter-spacing: normal;
-        text-align: right;
-        color: #1e2132;
-    }
-    div{
-        font-family: OpenSansHebrew;
-        font-size: 16px;
-        font-weight: normal;
-        font-stretch: normal;
-        font-style: normal;
-        line-height: normal;
-        letter-spacing: normal;
-        text-align: right;
-        color: #00094a;
-    }
-`;
-
-export const StyledGenderIcon = styled(GenderIcon)`
-
-
-`;
-
-export const StyledIcon = styled(Icon)`
-   font-family: "Material Icons" !important;
-    margin-top: 2%;
-    color: #002cb1;
-    height: 24px;
-    cursor: pointer;
-    user-select: none;
-`;
-export const StyledIconText = styled.div`
-    cursor: pointer;
-    user-select: none;
-    width: 132px;
-    height: 22px;
+  margin-top: 10px;
+  label {
+    margin-left: 10px;
+    margin-right: 0px;
+  }
+  span {
+    width: 86px;
+    height: 19px;
     font-family: OpenSansHebrew;
-    font-size: 16px;
+    font-size: 14px;
     font-weight: bold;
     font-stretch: normal;
     font-style: normal;
     line-height: normal;
     letter-spacing: normal;
     text-align: right;
-    color: #002cb1;
-    margin-top: 2.5%;
+    color: #000b40;
+    margin-right: 19px;
+  }
+  div {
+    width: 103px;
+    height: 19px;
+    font-family: OpenSansHebrew;
+    font-size: 14px;
+    font-weight: bold;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: normal;
+    letter-spacing: normal;
+    text-align: right;
+    color: #000b40;
+  }
 `;
-export const StyledValueComponent = styled.label`
-    width:100%;
-    margin-top: 20px;
-    margin-left: 25px;
-    opacity: 0.6;
+
+export const StyledLabelStatusAppointment = styled.label`
+  display: inline-flex;
+  /* max-width: 30%;
+    min-width: 30%;
+    height: 22px;*/
+  margin-top: 10px;
+  label {
+    margin-left: 5px;
+    margin-right: -12px;
+  }
+  span {
+    width: 80px;
+    height: 18px;
+    font-family: OpenSansHebrew;
+    font-size: 14px;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.29;
+    letter-spacing: normal;
+    text-align: right;
+
+    text-align: -webkit-center;
+    border-radius: 15px;
+    background: #eaf7ff;
+  }
+  div {
+    width: 50px;
+  }
+`;
+
+export const StyledLabelServiceTypeAppointment = styled.label`
+  display: inline-flex;
+  /* max-width: 30%;
+    min-width: 30%;
+    height: 22px;*/
+  margin-top: 10px;
+  label {
+    margin-left: 5px;
+    margin-right: -12px;
+  }
+  span {
+    width: 80px;
+    height: 18px;
+    font-family: OpenSansHebrew;
+    font-size: 14px;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.29;
+    letter-spacing: normal;
+  }
+  div {
+  }
+`;
+
+export const StyledLabelName = styled.label`
+  display: inline-table;
+  max-width: 24%;
+  min-width: 24%;
+  height: 22px;
+  margin-top: 10px;
+  span {
+    font-family: OpenSansHebrew;
+    font-size: 14px;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: normal;
+    letter-spacing: normal;
+    text-align: right;
+    color: #00094a;
+    margin: auto;
+  }
+  div {
+    font-family: OpenSansHebrew;
+    font-size: 14px;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: normal;
+    letter-spacing: normal;
+    text-align: right;
+    color: #00094a;
+  }
+`;
+
+export const StyledLabelTZ = styled.label`
+  display: inline-flex;
+  max-width: 30%;
+  min-width: 30%;
+  height: 22px;
+  margin-top: 10px;
+  span {
     font-family: OpenSansHebrew;
     font-size: 16px;
     font-weight: normal;
     font-stretch: normal;
     font-style: normal;
-    line-height: 1.13;
+    line-height: normal;
     letter-spacing: normal;
-    text-align: center;
-    color: navy;
+    text-align: right;
+    color: #000b40;
+  }
+  div {
+    font-family: OpenSansHebrew;
+    font-size: 16px;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: normal;
+    letter-spacing: normal;
+    text-align: right;
+    color: #000b40;
+  }
+`;
+
+export const StyledLabelPhone = styled.label`
+  display: inline-flex;
+  max-width: 30%;
+  min-width: 30%;
+  height: 22px;
+  margin-top: 10px;
+  span {
+    opacity: 0.6;
+    font-family: OpenSansHebrew;
+    font-size: 13px;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.38;
+    letter-spacing: normal;
+    text-align: right;
+    color: #1e2132;
+  }
+  div {
+    font-family: OpenSansHebrew;
+    font-size: 16px;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: normal;
+    letter-spacing: normal;
+    text-align: right;
+    color: #00094a;
+  }
+`;
+
+export const StyledLabelAge = styled.label`
+  display: inline-flex;
+  max-width: 20%;
+  min-width: 20%;
+  height: 22px;
+  margin-top: 10px;
+  span {
+    opacity: 0.6;
+    font-family: OpenSansHebrew;
+    font-size: 13px;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.38;
+    letter-spacing: normal;
+    text-align: right;
+    color: #1e2132;
+  }
+  div {
+    font-family: OpenSansHebrew;
+    font-size: 16px;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: normal;
+    letter-spacing: normal;
+    text-align: right;
+    color: #00094a;
+  }
+`;
+
+export const StyledGenderIcon = styled(GenderIcon)``;
+
+export const StyledIcon = styled(Icon)`
+  font-family: 'Material Icons' !important;
+  margin-top: 2%;
+  color: #002cb1;
+  height: 24px;
+  cursor: pointer;
+  user-select: none;
+`;
+export const StyledIconText = styled.div`
+  cursor: pointer;
+  user-select: none;
+  width: 132px;
+  height: 22px;
+  font-family: OpenSansHebrew;
+  font-size: 16px;
+  font-weight: bold;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  text-align: right;
+  color: #002cb1;
+  margin-top: 2.5%;
+`;
+export const StyledValueComponent = styled.label`
+  width: 100%;
+  margin-top: 20px;
+  margin-left: 25px;
+  opacity: 0.6;
+  font-family: OpenSansHebrew;
+  font-size: 16px;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.13;
+  letter-spacing: normal;
+  text-align: center;
+  color: navy;
 `;
 
 export const StyledLinkWithIconComponent = styled.div`
-    margin: 5px 63px;
-    a{
-      width: 73px;
-      height: 18px;
-      font-family: OpenSansHebrew;
-      font-size: 14px;
-      font-weight: normal;
-      font-stretch: normal;
-      font-style: normal;
-      line-height: 1.29;
-      letter-spacing: normal;
-      text-align: right;
-      color: #002398;
-     }
-    ${StyledIcon}{
-        width: 7px;
-        height: 17px;
-        object-fit: contain;
-    }
+  margin: 5px 63px;
+  a {
+    width: 73px;
+    height: 18px;
+    font-family: OpenSansHebrew;
+    font-size: 14px;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.29;
+    letter-spacing: normal;
+    text-align: right;
+    color: #002398;
+  }
+  ${StyledIcon} {
+    width: 7px;
+    height: 17px;
+    object-fit: contain;
+  }
 `;
 
 export const StyledEmptyDiv = styled.div`
-    width: 55px !important;
+  width: 55px !important;
 `;
 
 export const StyledTableTextCell = styled.div`
@@ -402,7 +392,7 @@ export const StyledTableTextCell = styled.div`
 `;
 
 export const StyledHeaderTableAppointment = styled.div`
-    width: auto;
+  width: auto;
   height: 24px;
   font-family: OpenSansHebrew;
   font-size: 18px;
@@ -413,13 +403,12 @@ export const StyledHeaderTableAppointment = styled.div`
   letter-spacing: normal;
   text-align: right;
   color: #000b40;
-  a{
-      float: left;
+  a {
+    float: left;
     width: auto;
   }
 `;
 
-export const StyledLabelTableButton= styled.label`
-
-    float:left
+export const StyledLabelTableButton = styled.label`
+  float: left;
 `;
