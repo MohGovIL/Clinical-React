3a8b4f930132d23401e983a622a32214ea69e980
yuriyge <yuriyge@matrix.co.il>
2020-05-11 12:02:50 +0300

    PC-322 improved pattern for validate email

--- src/Utils/Helpers/validation/patterns.js
+++ src/Utils/Helpers/validation/patterns.js
@@ -5,7 +5,7 @@
  */
 
 export const getEmailRegexPattern = () => {
-  return /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 };
 
 export const getCellPhoneRegexPattern = () => {
