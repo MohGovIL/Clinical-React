5a17a3b3dd1efe67d1ee4eb71ed9570954a4f579
idangi <idangi@matrix.co.il>
2020-05-13 17:27:23 +0300

    PC-1011

--- src/Components/Generic/EncounterSheet/PatientBackground/Style.js
+++ src/Components/Generic/EncounterSheet/PatientBackground/Style.js
@@ -1,5 +1,15 @@
 import styled from 'styled-components';
 
-const StyledPatientBackground = styled.div``;
+const StyledPatientBackground = styled.div`
+  display: flex;
+  flex-basis: 25%;
+  max-width: 25%;
+  margin: 120px 38px 32px 38px;
+  height: calc(100vh - 88px - 32px - 32px);
+  max-height: calc(100vh - 88px - 32px - 32px);
+  flex-direction: column;
+  background-color: #ffffff;
+  box-shadow: 0 0 10px 0 rgba(152, 151, 151, 0.3);
+`;
 
 export default StyledPatientBackground;
--- src/Components/Generic/EncounterSheet/Style.js
+++ src/Components/Generic/EncounterSheet/Style.js
@@ -1,9 +1,10 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const StyledEncounterSheet = styled.div`
-    width: 100%;
-    max-height: 100vh;
-    display: flex;
+  width: 100%;
+  max-height: 100vh;
+  display: flex;
+  background-color: #f4f4f4;
 `;
 
 export default StyledEncounterSheet;
