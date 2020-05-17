dfe977e3246663474cbffce033e8393fc6b4237f
Dror Golan <drorgo@matrix.co.il>
2020-05-17 16:59:25 +0300

    PC-872

--- src/Components/Generic/PopupComponents/PopupAppointmentsPerPatient/AppointmentsAndEncountersTables.js
+++ src/Components/Generic/PopupComponents/PopupAppointmentsPerPatient/AppointmentsAndEncountersTables.js
@@ -197,7 +197,7 @@ const AppointmentsAndEncountersTables = ({
             <TableHead>
               <TableRow>
                 <TableCell align='right'>{t("Encounter's hour")}</TableCell>
-                <TableCell align='right'>{t('Lab test type')}</TableCell>
+                <TableCell align='right'>{t('Reason for refferal')}</TableCell>
                 <TableCell align='center'>{t('Status')}</TableCell>
                 <TableCell align='right'></TableCell>
                 <TableCell align='right'></TableCell>
@@ -304,7 +304,7 @@ const AppointmentsAndEncountersTables = ({
             <TableHead>
               <TableRow>
                 <TableCell align='right'>{t('Appointment time')}</TableCell>
-                <TableCell align='right'>{t('Lab test type')}</TableCell>
+                <TableCell align='right'>{t('Reason for refferal')}</TableCell>
                 <TableCell align='center'>{t('Status')}</TableCell>
                 <TableCell align='right'></TableCell>
                 <TableCell align='right'></TableCell>
@@ -426,7 +426,7 @@ const AppointmentsAndEncountersTables = ({
             <TableHead>
               <TableRow>
                 <TableCell align='right'>{t('Date')}</TableCell>
-                <TableCell align='right'>{t('Lab test type')}</TableCell>
+                <TableCell align='right'>{t('Reason for refferal')}</TableCell>
                 <TableCell align='center'>{t('Status')}</TableCell>
                 <TableCell align='right'></TableCell>
                 <TableCell align='right'></TableCell>
