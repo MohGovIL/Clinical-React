6e7ea51199733214d8ca2b75cf736961fa05b99a
idangi <idangi@matrix.co.il>
2020-05-25 13:35:01 +0300

    Added routing from DrawThisTable

--- src/Assets/Elements/Header/Search/DrawThisTable/LinkComponentWithIcon.js
+++ src/Assets/Elements/Header/Search/DrawThisTable/LinkComponentWithIcon.js
@@ -2,24 +2,24 @@ import React from 'react';
 import Link from '@material-ui/core/Link';
 import { StyledIconValueComponent } from '../Style';
 import { useTranslation } from 'react-i18next';
+import { Link as LinkRouter } from 'react-router-dom';
+import { baseRoutePath } from 'Utils/Helpers/baseRoutePath';
 
-const LinkComponentWithIcon = ({ linkUrl, linkHeader, iconType ,mode}) => {
+const LinkComponentWithIcon = ({ linkUrl, linkHeader, iconType, mode }) => {
   const { t } = useTranslation();
-  return (
-
-      (mode ==="view" || mode ==="write") ?
-      <Link href='linkUrl'>
-          <label>
-              {t(linkHeader)}
-              <StyledIconValueComponent
-                  iconType={iconType ? iconType : 'arrow_back_ios'}
-              />
-          </label>
-      </Link>
-      : null
-
-
-  );
+  return mode === 'view' || mode === 'write' ? (
+    <Link
+      //   href='linkUrl'
+      component={LinkRouter}
+      to={`${baseRoutePath()}/generic/EncounterSheet`}>
+      <label>
+        {t(linkHeader)}
+        <StyledIconValueComponent
+          iconType={iconType ? iconType : 'arrow_back_ios'}
+        />
+      </label>
+    </Link>
+  ) : null;
 };
 
 export default LinkComponentWithIcon;
