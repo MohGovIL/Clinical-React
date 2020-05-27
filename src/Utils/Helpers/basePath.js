770b19c411aa363d91846cfa98c7dc862aa78552
shaharzi <shaharzi@matrix.co.il>
2020-05-27 18:39:32 +0300

    basePath file

--- src/Utils/Helpers/basePath.js
+++ src/Utils/Helpers/basePath.js
@@ -3,5 +3,8 @@
  * @returns {string} Returns the base path of the projects
  */
 export const basePath = () => {
+    if(typeof process.env.REACT_APP_API_BASE_URL !== 'undefined')
         return `${window.location.protocol}//${process.env.REACT_APP_API_BASE_URL}/`;
+    else
+        return `${window.location.protocol}//backend.${window.location.host}/`;
 };
