5acecc7bb6c351f8d30a0a8d6606e6a3bb73c7da
idangi <idangi@matrix.co.il>
2020-06-14 11:29:40 +0300

    Finished with Contact information

---
+++ src/Components/Generic/PatientAdmission/PatientDetailsBlock/ContactInformation/index.js
@@ -0,0 +1,410 @@
+import React, { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { StyledFormGroup, StyledDivider, StyledAutoComplete } from '../Style';
+import Title from 'Assets/Elements/Title';
+import { Tabs, Tab, InputAdornment, CircularProgress } from '@material-ui/core';
+import CustomizedTextField from 'Assets/Elements/CustomizedTextField';
+import { ExpandLess, ExpandMore } from '@material-ui/icons';
+import { Controller, useFormContext } from 'react-hook-form';
+import { getStreets, getCities } from 'Utils/Services/API';
+
+const ContactInformation = ({
+  postalCode,
+  patientPOBox,
+  streetNumber,
+  city,
+  streetName,
+}) => {
+  const { t } = useTranslation();
+  const {
+    control,
+    register,
+    unregister,
+    triggerValidation,
+    setValue,
+    errors,
+  } = useFormContext();
+  useEffect(() => {
+    register('contactInformationTabValue');
+    return () => {
+      unregister('contactInformationTabValue');
+    };
+  }, [register, unregister]);
+
+  const [contactInformationTabValue, setContactInformationTabValue] = useState(
+    0,
+  );
+  const contactInformationTabValueChangeHandler = (event, newValue) => {
+    setValue('contactInformationTabValue', newValue);
+    triggerValidation(['addressPostalCode', 'POBoxPostalCode']);
+    setContactInformationTabValue(newValue);
+  };
+
+  const [cities, setCities] = useState([]);
+  const [citiesOpen, setCitiesOpen] = useState(false);
+  const loadingCities = citiesOpen && cities.length === 0;
+  // Contact Information - streets var
+  const [streets, setStreets] = useState([]);
+  const [streetsOpen, setStreetsOpen] = useState(false);
+  const loadingStreets = streetsOpen && streets.length === 0;
+
+  const [addressCity, setAddressCity] = useState(
+    city ? { name: t(city), code: city } : {},
+  );
+  const [addressStreet, setAddressStreet] = useState(
+    streetName ? { name: t(streetName), code: streetName } : {},
+  );
+  // Contact Information - PObox city - var
+  const [POBoxCity, setPOBoxCity] = useState(
+    city ? { name: t(city), code: city } : {},
+  );
+  // Contact Information - address - vars
+  const [addressStreetNumber, setAddressStreetNumber] = useState('');
+
+  const [addressPostalCode, setAddressPostalCode] = useState(postalCode || '');
+
+  const [POBox, setPOBox] = useState(patientPOBox || '');
+
+  const [POBoxPostalCode, setPOBoxPostalCode] = useState(postalCode || '');
+  useEffect(() => {
+    if (!citiesOpen) {
+      setCities([]);
+    }
+    if (!streetsOpen) {
+      setStreets([]);
+    }
+  }, [citiesOpen, streetsOpen]);
+
+  useEffect(() => {
+    let active = true;
+
+    if (!loadingCities) {
+      return undefined;
+    }
+
+    (async () => {
+      try {
+        const cities = await getCities();
+        if (active) {
+          setCities(
+            Object.keys(cities.data).map((cityKey) => {
+              let cityObj = {};
+              cityObj.code = cities.data[cityKey];
+              cityObj.name = t(cities.data[cityKey]);
+
+              return cityObj;
+            }),
+          );
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    })();
+
+    return () => {
+      active = false;
+    };
+  }, [loadingCities]);
+  useEffect(() => {
+    let active = true;
+
+    if (!loadingStreets) {
+      return undefined;
+    }
+
+    (async () => {
+      try {
+        const streets = await getStreets(addressCity.code.split('_')[1]);
+        if (active) {
+          if (Object.keys(streets.data).length) {
+            setStreets(
+              Object.keys(streets.data).map((streetKey) => {
+                let streetObj = {};
+                streetObj.code = streets.data[streetKey];
+                streetObj.name = t(streets.data[streetKey]);
+
+                return streetObj;
+              }),
+            );
+          } else {
+            const emptyResultsObj = {
+              code: 'no_result',
+              name: t('No Results'),
+            };
+            const emptyResults = [emptyResultsObj];
+            setStreets(emptyResults);
+          }
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    })();
+
+    return () => {
+      active = false;
+    };
+  }, [loadingStreets]);
+
+  const onTextBlur = (value, setState) => {
+    setState(value);
+  };
+  return (
+    <React.Fragment>
+      <StyledFormGroup>
+        <Title
+          fontSize={'18px'}
+          color={'#000b40'}
+          label={t('Contact Information')}
+          bold
+        />
+        <StyledDivider variant={'fullWidth'} />
+        {/* Contact Information tabs */}
+        <Tabs
+          value={contactInformationTabValue}
+          onChange={contactInformationTabValueChangeHandler}
+          indicatorColor='primary'
+          textColor='primary'
+          variant='standard'
+          aria-label='full width tabs example'>
+          <Tab label={t('Address')} />
+          <Tab label={t('PO box')} />
+        </Tabs>
+        {/* Contact Information tabs - address */}
+        {contactInformationTabValue === 0 ? (
+          <React.Fragment>
+            {/* Contact Information - address - city */}
+            <StyledAutoComplete
+              id='addressCity'
+              open={citiesOpen}
+              onOpen={() => {
+                setCitiesOpen(true);
+              }}
+              onClose={() => {
+                setCitiesOpen(false);
+              }}
+              loading={loadingCities}
+              options={cities}
+              value={addressCity}
+              onChange={(event, newValue) => {
+                setAddressCity(newValue);
+              }}
+              getOptionLabel={(option) =>
+                Object.keys(option).length === 0 &&
+                option.constructor === Object
+                  ? ''
+                  : option.name
+              }
+              noOptionsText={t('No Results')}
+              loadingText={t('Loading')}
+              renderInput={(params) => (
+                <CustomizedTextField
+                  width={'70%'}
+                  name='addressCity'
+                  inputRef={register()}
+                  {...params}
+                  label={t('City')}
+                  InputProps={{
+                    ...params.InputProps,
+                    endAdornment: (
+                      <React.Fragment>
+                        <InputAdornment position={'end'}>
+                          {loadingCities ? (
+                            <CircularProgress color={'inherit'} size={20} />
+                          ) : null}
+                          {citiesOpen ? <ExpandLess /> : <ExpandMore />}
+                        </InputAdornment>
+                      </React.Fragment>
+                    ),
+                  }}
+                />
+              )}
+            />
+            {/* Contact Information - address - streets */}
+            <StyledAutoComplete
+              options={streets}
+              loading={loadingStreets}
+              open={streetsOpen}
+              onOpen={() => addressCity.name && setStreetsOpen(true)}
+              onClose={() => setStreetsOpen(false)}
+              id='addressStreet'
+              value={addressStreet}
+              onChange={(event, newValue) => {
+                setAddressStreet(newValue);
+              }}
+              getOptionLabel={(option) =>
+                Object.keys(option).length === 0 &&
+                option.constructor === Object
+                  ? ''
+                  : option.name
+              }
+              noOptionsText={t('No Results')}
+              loadingText={t('Loading')}
+              getOptionDisabled={(option) => option.code === 'no_result'}
+              renderInput={(params) => (
+                <CustomizedTextField
+                  width={'70%'}
+                  {...params}
+                  name='addressStreet'
+                  inputRef={register()}
+                  InputProps={{
+                    ...params.InputProps,
+
+                    endAdornment: (
+                      <InputAdornment position={'end'}>
+                        {loadingStreets ? (
+                          <CircularProgress color={'inherit'} size={20} />
+                        ) : null}
+                        {streetsOpen ? <ExpandLess /> : <ExpandMore />}
+                      </InputAdornment>
+                    ),
+                  }}
+                  label={t('Street')}
+                />
+              )}
+            />
+            {/* Contact Information - address - house number */}
+            <Controller
+              name={'addressStreetNumber'}
+              control={control}
+              defaultValue={addressStreetNumber || streetNumber}
+              onBlur={([event]) => {
+                onTextBlur(event.target.value, setAddressStreetNumber);
+                return event.target.value;
+              }}
+              as={
+                <CustomizedTextField
+                  width={'70%'}
+                  id={'addressStreetNumber'}
+                  label={t('House number')}
+                />
+              }
+            />
+            {/* Contact Information - address - postal code */}
+            <Controller
+              defaultValue={addressPostalCode || postalCode}
+              name={'addressPostalCode'}
+              key='addressPostalCode'
+              control={control}
+              onBlur={([event]) => {
+                onTextBlur(event.target.value, setAddressPostalCode);
+                return event.target.value;
+              }}
+              as={
+                <CustomizedTextField
+                  width={'70%'}
+                  id={'addressPostalCode'}
+                  label={t('Postal code')}
+                  type='number'
+                />
+              }
+              rules={{ maxLength: { value: 7 }, minLength: { value: 7 } }}
+              error={errors.addressPostalCode && true}
+              helperText={errors.addressPostalCode && 'יש להזין 7 ספרות'}
+            />
+          </React.Fragment>
+        ) : (
+          <React.Fragment>
+            {/* Contact Information - POBox - city */}
+            <StyledAutoComplete
+              name='POBoxCity'
+              id='POBoxCity'
+              open={citiesOpen}
+              onOpen={() => {
+                setCitiesOpen(true);
+              }}
+              onClose={() => {
+                setCitiesOpen(false);
+              }}
+              onChange={(event, newValue) => {
+                setPOBoxCity(newValue);
+              }}
+              value={POBoxCity}
+              loading={loadingCities}
+              options={cities}
+              getOptionLabel={(option) => option.name}
+              noOptionsText={t('No Results')}
+              loadingText={t('Loading')}
+              renderInput={(params) => (
+                <CustomizedTextField
+                  width={'70%'}
+                  {...params}
+                  name='POBoxCity'
+                  inputRef={register()}
+                  label={t('City')}
+                  InputProps={{
+                    ...params.InputProps,
+
+                    endAdornment: (
+                      <React.Fragment>
+                        <InputAdornment position={'end'}>
+                          {loadingCities ? (
+                            <CircularProgress color={'inherit'} size={20} />
+                          ) : null}
+                          {citiesOpen ? <ExpandLess /> : <ExpandMore />}
+                        </InputAdornment>
+                      </React.Fragment>
+                    ),
+                  }}
+                />
+              )}
+            />
+            {/* Contact Information - POBox - POBox */}
+            <Controller
+              name={'POBox'}
+              control={control}
+              defaultValue={POBox}
+              onBlur={([event]) => {
+                onTextBlur(event.target.value, setPOBox);
+                return event.target.value;
+              }}
+              as={
+                <CustomizedTextField
+                  width={'70%'}
+                  id={'POBox'}
+                  label={t('PO box')}
+                />
+              }
+            />
+            {/* Contact Information - POBox - postal code */}
+            <Controller
+              defaultValue={POBoxPostalCode || postalCode}
+              name={'POBoxPostalCode'}
+              key='POBoxPostalCode'
+              control={control}
+              onBlur={([event]) => {
+                onTextBlur(event.target.value, setPOBoxPostalCode);
+                return event.target.value;
+              }}
+              as={
+                <CustomizedTextField
+                  width={'70%'}
+                  id={'POBoxPostalCode'}
+                  label={t('Postal code')}
+                  InputLabelProps={{
+                    shrink: postalCode && true,
+                  }}
+                />
+              }
+              rules={{ maxLength: { value: 7 }, minLength: { value: 7 } }}
+              error={errors.POBoxPostalCode && true}
+              helperText={errors.POBoxPostalCode && 'יש להזין 7 ספרות'}
+            />
+          </React.Fragment>
+        )}
+      </StyledFormGroup>
+      <span>
+        {t('To find a zip code on the Israel post site')}
+        <a
+          href={
+            'https://mypost.israelpost.co.il/%D7%A9%D7%99%D7%A8%D7%95%D7%AA%D7%99%D7%9D/%D7%90%D7%99%D7%AA%D7%95%D7%A8-%D7%9E%D7%99%D7%A7%D7%95%D7%93/'
+          }
+          target={'_blank'}
+          rel='noopener noreferrer'>
+          {t('Click here')}
+        </a>
+      </span>
+    </React.Fragment>
+  );
+};
+
+export default ContactInformation;
