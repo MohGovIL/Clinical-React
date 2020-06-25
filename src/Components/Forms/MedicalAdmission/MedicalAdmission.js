d8f0604088671d9c20cf84a270aa6070d5d54d06
yuriyge <yuriyge@matrix.co.il>
2020-06-25 19:08:37 +0300

    EM-75 working 2

--- src/Components/Forms/MedicalAdmission/MedicalAdmission.js
+++ src/Components/Forms/MedicalAdmission/MedicalAdmission.js
@@ -3,7 +3,11 @@
 import { connect } from 'react-redux';
 import React, { useState } from 'react';
 import VisitDetails from '../../Generic/PatientAdmission/PatientDetailsBlock/VisitDetails';
-import { FormContext, useForm } from 'react-hook-form';
+import { FormContext, useForm, useFormContext } from 'react-hook-form';
+import { Grid } from '@material-ui/core';
+import StyledSwitch from '../../../Assets/Elements/StyledSwitch';
+import { StyledFormGroup } from '../../Generic/PatientAdmission/PatientDetailsBlock/Style';
+import { useTranslation } from 'react-i18next';
 
 const MedicalAdmission = ({
   patient,
@@ -14,10 +18,17 @@ const MedicalAdmission = ({
   verticalName,
   permission,
 }) => {
-  const methods = useForm({
-    mode: 'onBlur',
-    submitFocusError: true,
-  });
+  const { t } = useTranslation();
+  // const {
+  //   register,
+  //   control,
+  //   // requiredErrors,
+  //   setValue,
+  //   unregister,
+  //   reset,
+  //   getValues,
+  // } = useFormContext();
+
   const [requiredErrors, setRequiredErrors] = useState({
     selectTest: '',
     commitmentAndPaymentReferenceForPaymentCommitment: '',
@@ -28,6 +39,12 @@ const MedicalAdmission = ({
     ReferralFile: '',
     CommitmentFile: '',
   });
+
+  const methods = useForm({
+    mode: 'onBlur',
+    submitFocusError: true,
+  });
+
 
   return (
     <React.Fragment>
@@ -42,6 +59,38 @@ const MedicalAdmission = ({
             priority={encounter.priority}
           />
         </FormContext>
+      <Grid
+        container
+        direction={'row'}
+        justify={'flex-start'}
+        alignItems={'center'}>
+        <span>{t('Is urgent?')}</span>
+        {/* Requested service - switch */}
+        <StyledSwitch
+          name='isUrgent'
+          // register={register}
+          label_1={'No'}
+          label_2={'Yes'}
+          marginLeft={'40px'}
+          marginRight={'40px'}
+        />
+      </Grid>
+      <Grid
+        container
+        direction={'row'}
+        justify={'flex-start'}
+        alignItems={'center'}>
+        <span>{t('Insulation required?')}</span>
+        {/* Requested service - switch */}
+        <StyledSwitch
+          name='insulationRequired'
+          // register={register}
+          label_1={'No'}
+          label_2={'Yes'}
+          marginLeft={'40px'}
+          marginRight={'40px'}
+        />
+      </Grid>
     </React.Fragment>
   );
 };
