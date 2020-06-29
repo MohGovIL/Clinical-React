ec2e50eea8270cba2484a1f80fb51dd08d8c7e30
idangi <idangi@matrix.co.il>
2020-06-29 17:39:42 +0300

    Added permission to 'ROR' Component

--- src/Components/Forms/DiagnosisAndRecommendations/RecommendationsOnRelease /index.js
+++ src/Components/Forms/DiagnosisAndRecommendations/RecommendationsOnRelease /index.js
@@ -6,8 +6,10 @@ import { useTranslation } from 'react-i18next';
 import { StyledFormGroup } from 'Assets/Elements/StyledFormGroup';
 import { StyledButton } from 'Assets/Elements/StyledButton';
 import { StyledDivider } from '../Style';
+import { useFormContext } from 'react-hook-form';
 const RecommendationsOnRelease = () => {
   const { t } = useTranslation();
+  const { permission } = useFormContext();
   return (
     <StyledFormGroup>
       <Title label={t('Recommendations on release')} fontSize='22px' bold />
@@ -21,8 +23,10 @@ const RecommendationsOnRelease = () => {
           label={t('Instructions for further treatment')}
           width='45%'
           multiline
+          disabled={permission === 'view' ? true : false}
         />
         <StyledButton
+          disabled={permission === 'view' ? true : false}
           width='113px'
           height='32px'
           color='primary'
