963e9b43aad3c15c4f30953fd952c3b7dc8c6013
idangi <idangi@matrix.co.il>
2020-07-23 13:48:20 +0300

    Added a get method for MedicationRequest

--- src/Utils/Services/FHIR/MedicationRequest/index.js
+++ src/Utils/Services/FHIR/MedicationRequest/index.js
@@ -24,10 +24,51 @@ const MedicationRequestState = {
   },
   updateMedicationRequest: (params) => {},
   getMedicationRequest: (params) => {
-    return CRUDOperations(
-      'read',
-      `${params.url}?encounter=${params.encounterId}`,
-    );
+    /**
+     *  _id - If _id is supplied all other params will be ignored
+     *  encounterId
+     *  patientId
+     *  recorder
+     *  requester
+     *  code
+     */
+
+    // Side note in doWork I don't thin that you should take functionParams and insert them inside the object
+    // because then if you want to loop over only functionParams you can't you'll loop on the url as well for no reason
+    // Should be refactored in every other FHIR element
+    const availableSearchParams = {
+      encounterId: true,
+      patientId: true,
+      recorder: true,
+      requester: true,
+      code: true,
+    };
+    if (params._id) {
+      params.url += `/${params._id}`;
+    } else {
+      /**
+       * item inside searchParamsProvided: {searchParamName: "<NAME>", searchParamValue: "<VALUE>"}
+       */
+      const searchParamsProvided = [];
+      for (const paramsKey in params) {
+        if (params.hasOwnProperty(paramsKey)) {
+          const element = params[paramsKey];
+          if (availableSearchParams[paramsKey])
+            searchParamsProvided.push({
+              searchParamName: paramsKey,
+              searchParamValue: element,
+            });
+        }
+      }
+      if (searchParamsProvided.length) {
+        searchParamsProvided.forEach((param, paramIndex) => {
+          params.url += `${paramIndex === 0 ? '?' : '&'}${
+            param.searchParamName
+          }=${param.searchParamValue}`;
+        });
+      }
+    }
+    return CRUDOperations('read', `${params.url}`);
   },
 };
 
