46a79ce59d92b771930fd15323dc5f917092061b
idangi <idangi@matrix.co.il>
2020-08-09 13:59:32 +0300

    Finished with denormalizing MedicationStatement

--- src/Utils/Helpers/FhirEntities/denormalizeFhirEntity/denormalizeFhirMedicationStatement/index.js
+++ src/Utils/Helpers/FhirEntities/denormalizeFhirEntity/denormalizeFhirMedicationStatement/index.js
@@ -1,5 +1,7 @@
 const denormalizeFhirMedicationStatement = (medicationStatement) => {
   const denormalizedFhirMedicationStatement = {};
+  const category = {};
+  const effectivePeriod = {};
   for (const medicationStatementKey in medicationStatement) {
     if (medicationStatement.hasOwnProperty(medicationStatementKey)) {
       const element = medicationStatement[medicationStatementKey];
@@ -7,13 +9,60 @@ const denormalizeFhirMedicationStatement = (medicationStatement) => {
         case 'status':
           denormalizedFhirMedicationStatement['status'] = element;
           break;
-        case '':
+        case 'categoryCode':
+          category['coding'] = [
+            {
+              code: element,
+              system: medicationStatement.categorySystem,
+            },
+          ];
+          break;
+        case 'categoryText':
+          category['text'] = element;
+          break;
+        case 'medicationCodeableConceptCode':
+          denormalizedFhirMedicationStatement['medicationCodeableConcept'] = {
+            coding: [
+              {
+                system: medicationStatement.medicationCodeableConceptSystem,
+                code: element,
+              },
+            ],
+          };
+          break;
+        case 'patient':
+          denormalizedFhirMedicationStatement['subject'] = `Patient/${element}`;
+          break;
+        case 'effectivePeriodStart':
+          effectivePeriod['start'] = element;
+          break;
+        case 'effectivePeriodEnd':
+          effectivePeriod['end'] = element;
+          break;
+        case 'dateAsserted':
+          denormalizedFhirMedicationStatement['dateAsserted'] = element;
+          break;
+        case 'informationSource':
+          denormalizedFhirMedicationStatement['informationSource'] = element;
+          break;
+        case 'note':
+          denormalizedFhirMedicationStatement['note'] = [
+            {
+              text: element,
+            },
+          ];
           break;
         default:
           break;
       }
     }
   }
+
+  if (Object.keys(category).length)
+    denormalizedFhirMedicationStatement['category'] = category;
+
+  if (Object.keys(effectivePeriod).length)
+    denormalizedFhirMedicationStatement['effectivePeriod'] = effectivePeriod;
 
   return denormalizedFhirMedicationStatement;
 };
