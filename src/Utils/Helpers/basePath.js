6d139b2bd1e87c0cb355bc2a17c72545fadeb4eb
Amiel Elboim <amielel@matrix.co.il>
2020-11-19 22:51:16 +0200

    add option for prefix in the domain name

--- src/Utils/Helpers/basePath.js
+++ src/Utils/Helpers/basePath.js
@@ -3,8 +3,15 @@
  * @returns {string} Returns the base path of the projects
  */
 export const basePath = () => {
-    if(typeof process.env.REACT_APP_API_BASE_URL !== 'undefined')
+    if (typeof process.env.REACT_APP_API_BASE_URL !== 'undefined') {
         return `${window.location.protocol}//${process.env.REACT_APP_API_BASE_URL}/`;
-    else
+    }
+    if (typeof process.env.REACT_APP_API_HOST_PREFIX !== 'undefined') {
+        const parseDomain = window.location.hostname.split('.');
+        parseDomain.splice(-2, 0, process.env.REACT_APP_API_HOST_PREFIX);
+        const domain = parseDomain.join('.');
+        return `${window.location.protocol}//${domain}/`;
+    } else {
         return `${window.location.protocol}//backend.${window.location.host}/`;
+    }
 };
